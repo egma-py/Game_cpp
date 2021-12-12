5c069432968bb39f7bc24ce2734974ae9bf0b7a0
egma-py <mamenchikov.ev@phystech.edu>
2021-12-12 16:42:44 +0300

    test_1

--- TheGame/main.cpp
+++ TheGame/main.cpp
@@ -1,8 +1,53 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+class Hero {
+private:
+
+public:
+
+};
+
+class Enemy {
+private:
+
+public:
+
+};
+
+class Level{
+private:
+    sf::Color FloorColor_ = sf::Color(128, 129, 130);
+    sf::RectangleShape Field_;
+    std::pair<int, int> WindowSize_;
+public:
+    Level(std::pair<int, int> WindowSize) {
+        this->WindowSize_ = WindowSize;
+
+        float length = (10 * WindowSize.first) / 16;
+        float width = (7 * WindowSize.second) / 9;
+        this->Field_ = sf::RectangleShape(sf::Vector2f(length, width));
+        this->Field_.setFillColor(this->FloorColor_);
+
+        float x = (3 * WindowSize.first) / 16;
+        float y = (WindowSize.second) / 9;
+        this->Field_.move(x, y);
+    }
+
+    sf::RectangleShape Draw() {
+        return this->Field_;
+    }
+};
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(640, 360), "The Game");
+    std::pair<int, int> WindowSize;
+    WindowSize.first = 1080;
+    WindowSize.second = 720;
+
+    Level level(WindowSize);
+
+    sf::RenderWindow window(sf::VideoMode(WindowSize.first, WindowSize.second), "The Game");
     sf::CircleShape shape(100);
     shape.setFillColor(sf::Color::Blue);
 
@@ -11,12 +56,22 @@ int main()
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
+            switch (event.type) {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+
+                case sf::Event::MouseButtonPressed:
+                    std::cout << '(' << event.mouseButton.x << ", " << event.mouseButton.y << ')' << ' ';
+
+                default:
+                    break;
+
+            }
         }
 
         window.clear();
-        window.setFillColor(sf::Color::Green);
+        window.draw(level.Draw());
         window.draw(shape);
         window.display();
     }
